6b5ed02df4b4de39802b9fd7650e27bed33b54c3
pablo curell <pablocm90@gmail.com>
2017-11-06 17:36:28 +0100

    changed the registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
     def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
