dcedfcfd89d4680c2bff124574a046445a346f51
pablo curell <pablocm90@gmail.com>
2017-11-06 18:20:53 +0100

    I hope I am not fucking everything up

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,26 @@
+class ClientsController < ApplicationController
+
+  before_action :set_client, except: [:update]
+
+  def show
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def set_client
+    @client = Client.find(client.user = current_user)
+  end
+
+
+
+end
