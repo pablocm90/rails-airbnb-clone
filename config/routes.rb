640430151151906b65d5e6c58a95dcd1fb6f8f32
pablo curell <pablocm90@gmail.com>
2017-11-06 15:54:41 +0100

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'books#index'
+  resources :users, only: [] do
+    resources :authors, only: [ :new, :create ]
+    resources :book_transactions, only: [ :new, :create ]
+  end
+
+  resources :clients , except: [ :new, :create ] do
+    resources :book_transactions, only: [ :create, :new, :update ]
+  end
+  resources :authors, except: [ :new, :create ] do
+    resources :books, except: [ :index ]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
