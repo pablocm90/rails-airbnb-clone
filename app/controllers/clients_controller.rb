e11ec7a8021d87d90ca675fa0d8efbcaf39858ad
pablo curell <pablocm90@gmail.com>
2017-11-07 11:41:33 +0100

    information displays and can be edited

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,7 @@
 class ClientsController < ApplicationController
 
-  before_action :set_client, except: [:update]
-  before_action :set_user, except: [:update]
+  before_action :set_client
+  before_action :set_user
 
   def show
   end
@@ -10,6 +10,16 @@ class ClientsController < ApplicationController
   end
 
   def update
+    @client.profile_picture = client_params[:profile_picture]
+    @client.description = client_params[:description]
+    @client.prefered_genre = client_params[:prefered_genre]
+    @client.currency = client_params[:currency]
+    @client.birthday = client_params[:birthday]
+    if @client.save
+      redirect_to client_path(@client)
+    else
+      render 'show'
+    end
   end
 
   def delete
@@ -26,7 +36,7 @@ class ClientsController < ApplicationController
   end
 
   def client_params
-
+    params.require(:client).permit(:profile_picture, :description, :prefered_genre, :currency, :birthday)
   end
 
 
