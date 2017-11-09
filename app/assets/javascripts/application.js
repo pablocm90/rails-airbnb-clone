5cd25ec859f6d9fd2f2e8f16a1e7a76302082156
KarelVerhaeghe <karelverhaeghe@hotmail.com>
2017-11-09 17:59:56 +0100

    Pull

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
+//= require social-share-button
 //= require bootstrap-sprockets
 //= require notice_slide
 //= require Chart.bundle
