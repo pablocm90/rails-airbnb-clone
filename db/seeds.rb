1a3de2a63fb66d58e891cfa1a00622b3e708f990
pablo curell <pablocm90@gmail.com>
2017-11-10 14:33:39 +0100

    started reworking

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,48 @@
 require 'faker'
 
-p "erasing author, transactions and books"
-
+p "erasing everything"
+  Client.destroy_all
+  User.destroy_all
   BookTransaction.destroy_all
   Review.destroy_all
   Book.destroy_all
   Author.destroy_all
+  Review.destroy_all
 
-p "defining users"
+p "defining variables"
 
-@users = User.all
+password = "qwerty"
+emails = ["linnette@user", "green-dinosaur@user", "dutchness@user", "spaniard@user"]
+usernames = ["Linnette", "Green-dinosaur", "Dutchness", "Spaniard"]
+genres = ["fantasy", "crime", "historical-fiction", "science-fiction", "horror", "non-fiction"]
+
+p "creating users - 2 of them are authors, all of them are clients"
+
+usernames.each_with_index do |user, index|
+  person = User.new(pasword: password, email: emails[index], username: user)
+  Client.create(user: person)
+  if index.even?
+    person.is_author = true
+    person.save
+    if user == "linnette"
+      url = "https://pbs.twimg.com/profile_images/573545169569509376/O71ZFKB9_400x400.jpeg"
+      user_author = Author.create(user: person, description: Faker::HarryPotter.quote, currency: "eur")
+      user_author.remote_profile_picture_url = url
+      user_author.save
+    else
+      url = "https://media.licdn.com/media/AAIA_wDGAAAAAQAAAAAAAAn3AAAAJDU2N2IzYTJkLWY1OWUtNGJmYS05ODdiLTU5ZTgwZmM5ODExMQ.jpg"
+      user_author = Author.create(user: person, description: Faker::HarryPotter.quote, currency: "eur")
+      user_author.remote_profile_picture_url = url
+      user_author.save
+  else
+    person.save
+  end
+end
+
+
+
+
+
 
 p "defining clients"
 
